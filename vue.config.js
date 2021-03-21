c562f3560b5e40c99cd33adfa53a23b48d2e278f
aihoque2 <aihoque2@illinois.edu>
2021-03-21 16:26:10 -0400

    added vue config file

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    pages: {
+        'index' : {
+            entry: './src/pages/Home/main.js',
+            template: 'public/index.html',
+            title: 'Home',
+            chunks: ['chunk-vendors','chunk-common', 'index']
+        },
+
+        'about': {
+            entry: './src/pages/Home/main.js',
+            template: 'public/index.html',
+            title: 'About',
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
+        }
+    }
+}
