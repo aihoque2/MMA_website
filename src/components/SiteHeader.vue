fe24a0b316edfa1ae3aaf9005729cc001c8582d1
aihoque2 <aihoque2@illinois.edu>
2021-10-22 18:14:38 -0500

    added responsive-video-background-player

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,20 @@
+
+<template>
+
+</template>
+
+
+<script>
+//TODO: import the from vue-responsive-vide-backbground player
+import VideoBackground from 'vue-responsive-video-background-player'
+Vue.component('video-background', VideoBackground);
+
+
+export default {
+  //TODO
+  name: 'VideoBackground',
+  props: {
+    msg: String
+  }
+
+</script>
